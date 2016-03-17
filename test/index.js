e8ff7709601298aa517b160d4407c27ff434e733
Usman Tahir <ch.usman.tahir@gmail.com>
2016-03-18 02:24:11 +0500

    Test cases for Task 1 complete. i.e. Get titles using call back.

---
+++ test/index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var assert = require('assert');
+var cheerio = require('cheerio');
+var request = require('supertest');
+
+describe('Make API Call to http://localhost:3000/I/want/title', function () {
+    var server;
+    beforeEach(function () {
+        server = require('../lib/index');
+    });
+    afterEach(function () {
+        server.close();
+    });
+
+    it('should return heading with no titles found and status 200', function (done) {
+        request(server).get('/I/want/title').expect("Content-type", 'text/html; charset=utf-8').expect(200).end(function(err,res){
+            // HTTP status should be 200
+            assert.equal(res.status, 200);
+            assert.equal(res.text, '<h1>No Titles Found</h1>');
+
+            // Error key should be false.
+            assert.equal(res.error, false);
+            done();
+        });
+    });
+
+    it('should return one title because only one url is passed to it as a string', function (done) {
+        this.timeout(5000);
+        request(server).get('/I/want/title?address=blog.andrewray.me/how-to-debug-mocha-tests-with-chrome').expect("Content-type", 'text/html; charset=utf-8').expect(200).end(function(err,res){
+            // HTTP status should be 200
+            assert.equal(res.status, 200);
+
+            var $ = cheerio.load(res.text);
+
+            assert.deepEqual($('li span').text(), 'How to Debug Mocha Tests With Chrome');
+            assert.equal($('li span').length, 1);
+
+            // Error key should be false.
+            assert.equal(res.error, false);
+            done();
+        });
+    });
+
+    it('should return list of titles with urls parsed', function (done) {
+        this.timeout(5000);
+        request(server).get('/I/want/title?address=blog.andrewray.me/how-to-debug-mocha-tests-with-chrome&address=google.com').expect("Content-type", 'text/html; charset=utf-8').expect(200).end(function(err,res){
+            // HTTP status should be 200
+            assert.equal(res.status, 200);
+
+            var $ = cheerio.load(res.text),
+                titles = [];
+
+            assert.equal($('li span').length, 2);
+            $('li span').each(function(i, elem) {
+                titles[i] = $(this).text();
+            });
+
+            assert.equal(titles[0], 'How to Debug Mocha Tests With Chrome');
+            assert.equal(titles[1], 'Google');
+
+            // Error key should be false.
+            assert.equal(res.error, false);
+            done();
+        });
+    });
+});
--- test/utils.js
+++ test/utils.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert');
 var utils = require('../lib/utils');
+var cheerio = require('cheerio');
 
 describe('Prepare URL array', function () {
     it("should return array as is if array of url strings is passed to the function", function() {
@@ -26,5 +27,23 @@ describe('append protocol (http || https) to url', function () {
         var urls = ['google.com'];
         utils.appendProtocol(urls);
         assert.deepEqual(urls, ['http://google.com']);
+    });
+});
+
+describe('build html', function () {
+    it('should return html string with with li\'s appended with name and title if object with data is passed', function () {
+        var htmlString = utils.buildHtml([{url: 'http://google.com', title: 'Google'}]);
+        var $ = cheerio.load(htmlString);
+        assert($('li b').text(), 'http://google.com');
+    });
+});
+
+describe('get title against html', function () {
+    it('should return object with title and url', function () {
+        var title = utils.getTitle({request: {href: 'www.google.com'}}, '<html><title>Google</title></html>');
+        assert.deepEqual(title, {
+            'url': 'www.google.com',
+            'title': 'Google'
+        });
     });
 });
